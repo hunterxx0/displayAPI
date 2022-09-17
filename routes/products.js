fabce2bda47618c76b6ec41a7800c223223343da
hunterxx0 <azouzmhamed@gmail.com>
2022-09-17 15:06:58 +0100

    first

--- routes/products.js
+++ routes/products.js
@@ -8,10 +8,14 @@ const router = express.Router();
 // get routes:
 //		get all products
 router.get('/', async (req, res) => {
+    const limit = parseInt(req.query.limit, 10) || 20;
+    const sortRev = parseInt(req.query.reverse, 10) || -1;
+    const sortVal = req.query.sort || 'title';
 	try {
 		const products = await Product.find()
 		.select('-seller_id')
-		.sort(req.query.sort || 'title');
+		.sort({ [sortVal] : sortRev})
+		.limit(limit);
 		res.json(products);
 	} catch (err) {
 		res.status(500).json({message: err.message});
