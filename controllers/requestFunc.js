f46d17cf0f372d76be56d8bf863d630c69f12f65
hunterxx0 <azouzmhamed@gmail.com>
2022-09-18 16:27:13 +0100

    first

--- controllers/requestFunc.js
+++ controllers/requestFunc.js
@@ -8,10 +8,9 @@ export async function  requestAdd(req, res) {
 	let request = req.body;
 	const myuuid = v4();
 	request.id = myuuid;
-	const user = getUser(request.user_id);
+	const user = await getUser(request.user_id);
 	console.log(user);
 	console.log('------------');
-	console.log(res.product);
 	try {
 		
 		if (!user) {
