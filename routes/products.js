28cea4b4346ae5c014b577086fde28223db26fce
hunterxx0 <azouzmhamed@gmail.com>
2022-10-02 19:52:22 +0100

    first

--- routes/products.js
+++ routes/products.js
@@ -141,9 +141,9 @@ router.get('/sellers/category/:category', async (req, res) => {
 	try {
 		const products = await Product.find({"category": req.params.category})
 		.select('seller_name -_id');
-
+		/*
 		const result = [...new Set(products.map(a => a.seller_name))];
-		
+		*/
 		res.json(result);
 	} catch (err) {
 		res.status(500).json({message: err.message});
