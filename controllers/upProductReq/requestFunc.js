267ddbd44a204da72fcca427495d83122865fd0d
hunterxx0 <azouzmhamed@gmail.com>
2022-10-04 12:09:33 +0100

    first

--- controllers/upProductReq/requestFunc.js
+++ controllers/upProductReq/requestFunc.js
@@ -1,7 +1,7 @@
 import { v4 } from 'uuid';
 import mongoose from "mongoose";
-import {Product} from '../models/product.js';
-import {User} from '../models/user.js';
+import {Product} from '../../models/product.js';
+import {User} from '../../models/user.js';
 
 
 export async function  requestAdd(req, res) {
