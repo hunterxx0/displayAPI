66e562f0d5b55c6e013e983dd8ca98ffcc0a3578
hunterxx0 <azouzmhamed@gmail.com>
2022-10-14 11:49:30 +0100

    first

--- controllers/getProductReqs/getOneReq.js
+++ controllers/getProductReqs/getOneReq.js
@@ -4,13 +4,15 @@ export async function getOneReq(req, res) {
 	if (req.query.visit) {
 		try {
 			const user = await User.findById(req.query.visit);
-			if (!user) return res.json(res.product);
-			user.recently_viewed.push(res.product._id);
+			if (user) {
+				user.recently_viewed.push(res.product._id);
+				await user.save();
+			}
 			if (res.product.views) res.product.views += 1;
 			else res.product.views = 1;
 			await res.product.save();
-			await user.save();
 		} catch (err) {
+			console.log(err);
 			res.json(res.product);
 		}
 	}
