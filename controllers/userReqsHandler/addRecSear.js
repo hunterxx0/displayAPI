a732d4f21125777915d38cc84ba7d98f423e8eac
hunterxx0 <azouzmhamed@gmail.com>
2022-10-15 16:47:09 +0100

    first

--- controllers/userReqsHandler/addRecSear.js
+++ controllers/userReqsHandler/addRecSear.js
@@ -3,9 +3,15 @@ export async function addRecSear(req, res) {
 	res.user.recently_searched.unshift(req.params.keyword);
 	if (res.user.recently_searched.length != new Set(res.user.recently_searched).size) {
 		const findDuplicates = arr => arr.filter((item, index) => arr.indexOf(item) !== index)
+		console.log('dup:');
 		const index = findDuplicates(res.user.recently_searched);
+		console.log(index);
+		console.log('bef:');
+		console.log(res.user.recently_searched);
+		console.log('aft:');
 		if (index && index.length > 1)
 			res.user.recently_searched.splice(index[1], 1);
+		console.log(res.user.recently_searched);
 	}
 	if (res.user.recently_searched.length > 20) res.user.recently_searched.pop();
 	try {
