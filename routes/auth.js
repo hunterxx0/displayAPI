c994b1d18d012871a54b886d1bf538a850a62511
hunterxx0 <azouzmhamed@gmail.com>
2022-10-17 14:53:15 +0100

    first

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ router.post('/signupSeller', AuthController.sellerSignup);
 router.post('/signup', AuthController.signup);
 router.post('/login', AuthController.login);
 router.patch('/editlogin', AuthController.editLogin);
-router.delete('/eraseUser', AuthController.deleteUser);
+router.delete('/eraseUser/:id', AuthController.deleteUser);
 
 /*
 import {UserController} from '../controllers/UserController.js';
