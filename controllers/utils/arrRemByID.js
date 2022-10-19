69cc0018aeee909e6540b3216ab87691e8fbb722
hunterxx0 <azouzmhamed@gmail.com>
2022-10-19 20:36:53 +0100

    first

--- controllers/utils/arrRemByID.js
+++ controllers/utils/arrRemByID.js
@@ -1,8 +1,7 @@
 // remove a element by ID from an array of objects
 
 export async function arrRemByID(arr, id) {
-    const arrCopy = Array.from(arr);
-    const objWithIdIndex = arrCopy.findIndex((obj) => obj.id === id);
-    if (objWithIdIndex !== -1) arrCopy.splice(objWithIdIndex, 1);
-    return arrCopy;
+    const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
+    if (objWithIdIndex !== -1) arr.splice(objWithIdIndex, 1);
+    return arr;
 }
