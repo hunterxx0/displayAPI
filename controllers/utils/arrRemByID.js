379fc8eaccccf69717ddf29ce15d38dc078a392d
hunterxx0 <azouzmhamed@gmail.com>
2022-10-19 21:25:03 +0100

    first

--- controllers/utils/arrRemByID.js
+++ controllers/utils/arrRemByID.js
@@ -2,6 +2,10 @@
 
 export async function arrRemByID(arr, id) {
     const objWithIdIndex = arr.findIndex((obj) => obj.id === id);
+    console.log(arr);
+    console.log('objWithIdIndex:...........');
+    console.log(objWithIdIndex);
     if (objWithIdIndex !== -1) arr.splice(objWithIdIndex, 1);
+    console.log(arr);
     return arr;
 }
