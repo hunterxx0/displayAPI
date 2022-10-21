e37a8e3d29bd8f9369d1c440355b7f3b4861ee97
hunterxx0 <azouzmhamed@gmail.com>
2022-10-22 00:06:46 +0100

    first

--- controllers/userReqsHandler/getUnreadNotif.js
+++ controllers/userReqsHandler/getUnreadNotif.js
@@ -1,5 +1,5 @@
 //get all notifications
 
 export function getUnreadNotif(req, res) {
-    res.json(res.user.notifications.map(notif => { if (notif.read) return notif }));
+    res.json(res.user.notifications.map(notif => { if (!notif.read) return notif }));
 }
