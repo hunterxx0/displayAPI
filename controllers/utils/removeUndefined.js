e5ee92d82c6dbe433d94c7574d2d4d53f2d8bb37
hunterxx0 <azouzmhamed@gmail.com>
2022-10-22 13:42:58 +0100

    first

--- controllers/utils/removeUndefined.js
+++ controllers/utils/removeUndefined.js
@@ -4,7 +4,7 @@ export function removeUndefined(obj) {
     Object.keys(obj).forEach(key => {
         console.log(`key:${key}\nvalue:${obj[key]}\nvalType:${typeof obj[key]}\ndel:${obj[key] === undefined || obj[key] === null}\n++:${typeof obj[key] === Object}\n------\n`)
         if (obj[key] === undefined || obj[key] === null) delete obj[key];
-        else if (typeof obj[key] === Object) removeUndefined(obj[key]);
+        else if (typeof obj[key] === object || typeof obj[key] === Object) removeUndefined(obj[key]);
     });
     return obj;
 }
