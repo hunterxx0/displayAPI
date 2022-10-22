5eed637fa0cc14eb957d2c791334f32240ff2f6b
hunterxx0 <azouzmhamed@gmail.com>
2022-10-22 12:10:48 +0100

    first

--- models/user.js
+++ models/user.js
@@ -1,83 +1,123 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	fullName: {
-		type: String,
-		required: true,
-	},
-	email: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	hashedPassword: {
-		type: String,
-		required: true,
-	},
-	phone_number: {
-		type: Number,
-		required: true,
-	},
-	token: {
-		type: String,
-		required: true,
-		default: null
-	},
-	created_at: {
-		type: Date,
-		required: true,
-		default: Date.now
-	},
-	updated_at: {
-		type: Date,
-		required: false,
-	},
-	avatarURL: {
-		type: String,
-		required: false,
-	},
-	favorites: {
-		type: Array,
-		required: false,
-		default: []
-	},
-	requests: {
-		type: Array,
-		required: false,
-		default: []
-	},
-	recently_viewed: {
-		type: Array,
-		required: false,
-		default: []
-	},
-	recently_searched: {
-		type: Array,
-		required: false,
-		default: []
-	},
-	notifications: {
-		type: Array,
-		required: false,
-		default: []
-	},
-	following: {
-		type: Array,
-		required: false,
-		default: []
-	},
-	unfollowed: {
-		type: Array,
-		required: false,
-		default: []
-	},
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    hashedPassword: {
+        type: String,
+        required: true,
+    },
+    phone_number: {
+        type: Number,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+        default: null
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        required: false,
+    },
+    avatarURL: {
+        type: String,
+        required: false,
+    },
+    favorites: {
+        type: Array,
+        required: false,
+        default: []
+    },
+    requests: {
+        type: Array,
+        required: false,
+        default: []
+    },
+    recently_viewed: {
+        type: Array,
+        required: false,
+        default: []
+    },
+    recently_searched: {
+        type: Array,
+        required: false,
+        default: []
+    },
+    notifications: {
+        id: {
+            type: String,
+            required: true,
+        },
+        date: {
+            type: Number,
+            required: true,
+        },
+        prodID: {
+            type: String,
+            required: true,
+        },
+        product_name: {
+            type: String,
+            required: true,
+        },
+        read: {
+            type: String,
+            required: true,
+            default: 'unRead'
+        },
+        seller_name: {
+            type: String,
+            required: true,
+        },
+        Operation: {
+            type: String,
+            required: true,
+        },
+        targets: {
+            name: {
+                type: String,
+                required: false,
+            },
+            from: {
+                type: String,
+                required: false,
+            },
+            to: {
+                type: String,
+                required: false,
+            },
+        },
+    },
+    following: {
+        type: Array,
+        required: false,
+        default: []
+    },
+    unfollowed: {
+        type: Array,
+        required: false,
+        default: []
+    },
 })
 
 const User = mongoose.model('user', userSchema);
 
-export { User } ;
+export { User };
