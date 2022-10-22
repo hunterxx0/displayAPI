94e25c03929626c4d733b493d8f49861896aa56c
hunterxx0 <azouzmhamed@gmail.com>
2022-10-22 17:00:23 +0100

    first

--- controllers/userReqsHandler/addFav.js
+++ controllers/userReqsHandler/addFav.js
@@ -10,7 +10,7 @@ export async function addFav(req, res) {
         const seller = await Seller.findOne({ name: product.seller_name });
         seller.notifications.unshift(pushSellerNotif(
             res.user._id,
-            res.product,
+            product,
             'favorite'));
         await seller.save();
         res.user.favorites.push(req.params.favorite);
