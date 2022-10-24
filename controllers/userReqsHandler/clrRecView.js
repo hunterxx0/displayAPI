0f5fad2afe16df270f564dbbf3d8a944d868bffb
hunterxx0 <azouzmhamed@gmail.com>
2022-10-24 21:47:54 +0100

    first

--- controllers/userReqsHandler/clrRecView.js
+++ controllers/userReqsHandler/clrRecView.js
@@ -1,10 +1,14 @@
 //clear recently viewed products
 
 export async function clrRecView(req, res) {
+    console.log('1');
     if (res.user.notifications.length) {
         res.user.recently_viewed = [];
+        console.log('2');
         try {
+            console.log('3');
             const upUser = await res.user.save();
+            console.log('4');
             res.json(upUser);
         } catch (err) {
             console.log(err);
