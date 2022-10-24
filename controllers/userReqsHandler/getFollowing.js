51c16cee40c70e52c5d08a2af9edbd757d42b671
hunterxx0 <azouzmhamed@gmail.com>
2022-10-24 16:50:33 +0100

    first

--- controllers/userReqsHandler/getFollowing.js
+++ controllers/userReqsHandler/getFollowing.js
@@ -5,7 +5,7 @@ export async function getFollowing(req, res) {
     try {
         let sellers = null;
         if (res.user.following && res.user.following.length) {
-            sellers = await Seller.find({ _id: { $in: res.user.following } })
+            sellers = await Seller.find({ name: { $in: res.user.following } })
                 .select('_id name avatarURL');
         }
         res.json(sellers);
