7c4e6829d64c7d712c852dc67a2f2f6e351550e8
hunterxx0 <azouzmhamed@gmail.com>
2022-10-25 21:42:44 +0100

    first

--- controllers/userReqsHandler/getUsers.js
+++ controllers/userReqsHandler/getUsers.js
@@ -2,7 +2,9 @@ import {User} from '../../models/user.js';
 
 export async function getUsers (req, res) {
 	try {
-		const users = await User.find().sort(req.query.sort || 'created_at');
+		const users = await User.find()
+		.select('-__v -hashedPassword -token')
+		.sort(req.query.sort || 'created_at');
 		res.json(users);
 	} catch (err) {
 		res.status(500).json({message: err.message});
