739680888a5be57f4cc48dbab463e66346d9b18d
hunterxx0 <azouzmhamed@gmail.com>
2022-10-25 17:12:51 +0100

    first

--- controllers/dbFill/sellersProd.js
+++ controllers/dbFill/sellersProd.js
@@ -3,6 +3,7 @@ import https from 'https';
 import { Product } from '../../models/product.js';
 import { User } from '../../models/user.js';
 import { Seller } from '../../models/seller.js';
+import {AuthController} from '../auth/AuthController.js';
 
 export async function sellersProd(req, res) {
     let data = '';
@@ -10,11 +11,21 @@ export async function sellersProd(req, res) {
         path: `/products`,
         hostname: "ebay-api-get.herokuapp.com",
         method: 'GET'
-    }, ress => {
+    }, async ress => {
         ress.on('data', chunk => { data += chunk })
         ress.on('end', () => {
             console.log('data');
             const result = JSON.parse(data);
+            const req = {}
+            req.body = {
+            	username: "test",
+            	email: "test@test.com",
+            	password: "123456",
+            	phoneNumber: "123456789",
+            	seller_country: "BE",
+            	website: "test.com",
+            }
+            await AuthController.sellerSignup(req, res)
             res.json(result);
 
         })
