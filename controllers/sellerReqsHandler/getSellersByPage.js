f3a995e1eb6d9bcb739b9e3068a2458c1e57cfb7
hunterxx0 <azouzmhamed@gmail.com>
2022-10-27 12:34:45 +0100

    first

---
+++ controllers/sellerReqsHandler/getSellersByPage.js
@@ -0,0 +1,31 @@
+// get all sellers by page
+import { Seller } from '../../models/seller.js';
+
+export async function getSellersByPage(req, res) {
+    const page = parseInt(req.params.number, 10) || 1;
+    if (page <= 0) return res.status(400).json({ message: 'Bad Request' });
+    const limit = parseInt(req.query.limit, 10) || 20;
+    const sortRev = parseInt(req.query.reverse, 10) || -1;
+    const sortVal = req.query.sort || 'name';
+    const result = {}
+    try {
+        const sellersCount = await Seller.find().count();
+        if (!sellersCount) return res.json(result);
+        result.totalpages = Math.floor(sellersCount / limit);
+        if (sellersCount % limit) result.totalpages += 1;
+        const users = await Seller.find()
+            .select('-__v -hashedPassword -token')
+            .sort({
+                [sortVal]: sortRev
+            })
+            .skip((page - 1) * limit)
+            .limit(limit);
+        if (page > result.totalpages || page < 0) throw 'Bad Request';
+        result.nextPage = (page === result.totalpages) ? null : page + 1;
+        result.prevPage = (page === 1) ? null : page - 1;
+        result.data = products;
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+}
