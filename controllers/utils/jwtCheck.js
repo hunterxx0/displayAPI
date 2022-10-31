7f31086f6331074eefe556b7fade59947d995c64
hunterxx0 <azouzmhamed@gmail.com>
2022-11-01 00:36:35 +0100

    first

--- controllers/utils/jwtCheck.js
+++ controllers/utils/jwtCheck.js
@@ -28,6 +28,7 @@ export async function JWTAuth(req, res, next) {
         let { users } = await client.queryUsers({ id: user_id });
         if (!users.length) {
             user_id = encrDecr(decoded.user_id, 'decode');
+            console.log(user_id);
             let { users } = await client.queryUsers({ id: user_id });
         }
         if (!users.length || (users[0].role !== 'seller' && users[0].role !== 'admin'))
