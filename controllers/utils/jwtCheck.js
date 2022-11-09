30065a8c4cc1e4c5ac1ae2e902a8a553afb220e2
hunterxx0 <azouzmhamed@gmail.com>
2022-11-09 11:58:50 +0100

    first

--- controllers/utils/jwtCheck.js
+++ controllers/utils/jwtCheck.js
@@ -18,7 +18,6 @@ export async function JWTAuth(req, res, next) {
     token = token.slice(7);
     verify(token, api_secret, async (err, decoded) => {
         if (err) {
-            console.log(`JWT verify Error:\n\n\n${err}\n\n\n`);
             if (err.name === 'TokenExpiredError')
                 return res.status(401).json({ message: "Relogin" });
             return res.status(401).json({ message: "Unauthorized" });
@@ -28,9 +27,10 @@ export async function JWTAuth(req, res, next) {
         if (!users.length || (users[0].role !== 'seller' && users[0].role !== 'admin'))
             return res.status(401).json({ message: "Unauthorized" });
         if (users[0].role === 'seller') {
-            let seller = null;
-            let seller_name = req.body.seller_name;
-            if (!seller_name) seller_name = res.product.seller_name;
+            let seller = seller_name = null;
+            if (req.body.seller_name) seller_name = req.body.seller_name;
+            else if (!seller_name && res.product.seller_name) seller_name = res.product.seller_name;
+            else seller_name = users[0].name;
             if (!res.seller) {
                 seller = await Seller.findOne({ name: seller_name });
                 res.seller = seller;
